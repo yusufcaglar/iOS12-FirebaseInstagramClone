7aaded1476a47e892365c95d24292dfc16515fdb
Yusuf ÇAĞLAR <y.caglar17@icloud.com>
2018-10-19 16:28:32 +0300

    Initial Commit

---
+++ FirebaseInstaClone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FirebaseInstaClone
+//
+//  Created by Yusuf ÇAĞLAR on 19/10/2018.
+//  Copyright © 2018 Yusuf ÇAĞLAR. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
